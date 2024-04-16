16e03faf6043eb19a2cd6479fbe05c4a90a052c3
Crowdbotics <team@crowdbotics.com>
2024-04-16 17:39:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "laverne-care-giver-47540",
+  url: "https://laverne-care-giver-47540.botics.co",
+  api: "https://laverne-care-giver-47540.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
